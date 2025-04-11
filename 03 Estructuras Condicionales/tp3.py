5d6c28567e1a08de6962fdf2e56cd2bc62442f7d
rickyroure <127377831+rickyroure@users.noreply.github.com>
2025-04-11 18:27:32 -0300

    Update tp3.py

--- 03 Estructuras Condicionales/tp3.py
+++ 03 Estructuras Condicionales/tp3.py
@@ -1,1 +1,9 @@
-print(4 < 5)
+EDAD_MINIMA = 21
+
+edad = int(input("Ingrese su edad: ")) 
+categoria = input("Igrese su categoria(A, B, C, D, E, F, G): ")
+
+if edad >= EDAD_MINIMA and (categoria == "D" or "d"):
+    print("Prueba de conducir ambulancia")
+else:
+    print("No puede conducir ambulancia")
