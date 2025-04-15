b9f587992d90c43fdadaf1af4c77975465180729
rickyroure <rickyroure10@gmail.com>
2025-04-14 23:24:52 -0300

    Update MI-APELLIDO_TP-04.py

--- 04 Estructuras Repetitivas/MI-APELLIDO_TP-04.py
+++ 04 Estructuras Repetitivas/MI-APELLIDO_TP-04.py
@@ -1,2 +1,14 @@
-for x in range(1,11):
+for x in range(1,11,2):
     print(f"{x}")
+
+umbral = 10
+sumatoria = 0
+cont = 0
+
+while sumatoria < umbral:
+    cont += 1
+    num = int(input("Ingrese un numero: "))
+    sumatoria += num
+    
+
+print("El primedio es",(sumatoria / cont))
