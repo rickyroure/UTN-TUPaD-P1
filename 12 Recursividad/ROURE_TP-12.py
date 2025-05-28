f3bee3d787c22ca00bd98d36aab8c4d5c4d6a352
rickyroure <rickyroure10@gmail.com>
2025-05-28 13:00:33 -0300

    Update ROURE_TP-12.py

---
+++ 12 Recursividad/ROURE_TP-12.py
@@ -0,0 +1,35 @@
+#1) Crea una función recursiva que calcule el factorial de un número. Luego, utiliza esa
+#función para calcular y mostrar en pantalla el factorial de todos los números enteros
+#entre 1 y el número que indique el usuario
+
+
+#2) Crea una función recursiva que calcule el valor de la serie de Fibonacci en la posición
+#indicada. Posteriormente, muestra la serie completa hasta la posición que el usuario
+#especifique.
+
+
+#3) Crea una función recursiva que calcule la potencia de un número base elevado a un
+#exponente
+
+
+#4) Crear una función recursiva en Python que reciba un número entero positivo en base
+#decimal y devuelva su representación en binario como una cadena de texto
+
+
+#5) Implementá una función recursiva llamada es_palindromo(palabra) que reciba una
+#cadena de texto sin espacios ni tildes, y devuelva True si es un palíndromo o False si no
+#lo es.
+
+
+#6) Escribí una función recursiva en Python llamada suma_digitos(n) que reciba un
+#número entero positivo y devuelva la suma de todos sus dígitos.
+
+
+#7) Un niño está construyendo una pirámide con bloques. En el nivel más bajo coloca n
+#bloques, en el siguiente nivel uno menos (n - 1), y así sucesivamente hasta llegar al
+#último nivel con un solo bloque
+
+
+#8) Escribí una función recursiva llamada contar_digito(numero, digito) que reciba un
+#número entero positivo (numero) y un dígito (entre 0 y 9), y devuelva cuántas veces
+#aparece ese dígito dentro del número.
